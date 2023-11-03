6bd9c062ed8b9b4cb3d7d51f92c210f50b6ff804
Ứng Hùng <111266351+unghung18@users.noreply.github.com>
2023-11-03 20:32:35 +0700

    Update axiosClient.js

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://livechat-server.onrender.com/api',
     headers: {
         'content-type': 'application/json',
     },
